70cc8f36bcfe66996e1c81148c70c4bafa94a8e2
katriel <katrielaraujo@gmail.com>
2025-09-07 14:58:12 -0300

    Pagina de sem certificados gerados

--- src/app/_components/base-ui/base-ui.component.ts
+++ src/app/_components/base-ui/base-ui.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-base-ui',
+  standalone: true,
   imports: [],
   templateUrl: './base-ui.component.html',
   styleUrl: './base-ui.component.css'
